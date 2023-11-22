ce09c1bc36fd837061b484491654d91e53ceec42
shkim4548 <shkim4548@gmail.com>
2023-11-22 15:32:16 +0900

    BT 만드는 중

--- Source/HauntedMension/Ai/SevarogAIController.cpp
+++ Source/HauntedMension/Ai/SevarogAIController.cpp
@@ -10,12 +10,13 @@
 ASevarogAIController::ASevarogAIController() 
 {
 	// Blackboard�� �����´�
-	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BT(TEXT(""));
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BT(TEXT("/Script/AIModule.BehaviorTree'/Game/HauntedMansion/Ai/BT_Sevarog.BT_Sevarog"));
 	if (BT.Succeeded()) {
+		UE_LOG(LogTemp, Warning, TEXT("BB Binding"));
 		BehaviorTree = BT.Object;
 	}
 
-	static ConstructorHelpers::FObjectFinder<UBlackboardData> BD(TEXT(""));
+	static ConstructorHelpers::FObjectFinder<UBlackboardData> BD(TEXT("/Script/AIModule.BlackboardData'/Game/HauntedMansion/Ai/BB_Sevarog.BB_Sevarog"));
 	if (BD.Succeeded()) {
 		BlackboardData = BD.Object;
 	}
@@ -23,14 +24,34 @@ ASevarogAIController::ASevarogAIController()
 
 void ASevarogAIController::OnPossess(APawn* InPawn)
 {
+	Super::OnPossess(InPawn);
+	UBlackboardComponent* BlackboardComp = Blackboard;
+	if (UseBlackboard(BlackboardData, BlackboardComp))
+	{
+		if (RunBehaviorTree(BehaviorTree)) 
+		{
+			// 성공시 BT를 사용하라
+		}
+	}
 }
 
 void ASevarogAIController::OnUnPossess()
 {
-
+	Super::OnUnPossess();
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
 }
 
 void ASevarogAIController::RandomMove()
 {
+	auto CurrentPawn = GetPawn();
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetNavigationSystem(GetWorld());
 
+	if (NavSystem == nullptr)
+		return;
+
+	FNavLocation RandomLocation;
+	if (NavSystem->GetRandomPointInNavigableRadius(FVector::ZeroVector, 500.0f, RandomLocation))
+	{
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, RandomLocation);
+	}
 }
